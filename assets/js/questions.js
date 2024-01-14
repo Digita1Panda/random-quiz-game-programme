c3b89913a8b06eab6b1b054d6c238ba467d3a3dd
Digita1Panda <prodigykyo@gmail.com>
2024-01-14 02:26:03 +0000

    added files and start of question

---
+++ assets/js/questions.js
@@ -0,0 +1,9 @@
+let questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choice1: "strings",
+    choice2: "booleans",
+    choice3: "alerts",
+    choice4: "numbers",
+  },
+];
