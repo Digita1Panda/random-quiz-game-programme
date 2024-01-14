656472cdb6f9906d8161e720ae939ceb468a549a
Digita1Panda <prodigykyo@gmail.com>
2024-01-14 02:34:25 +0000

    second question added

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,5 +5,15 @@ let questions = [
     choice2: "booleans",
     choice3: "alerts",
     choice4: "numbers",
+    answer: 3,
+  },
+  {
+    question:
+      "The condition of in an if/else statement is enclosed within _____.",
+    choice1: "quotes",
+    choice2: "curly brackets",
+    choice3: "parentheses",
+    choice4: "square brackets",
+    answer: 2,
   },
 ];
