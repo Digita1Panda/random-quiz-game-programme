faec7981a0991ebde8537592d61ea7dbda924b0d
Digita1Panda <prodigykyo@gmail.com>
2024-01-15 02:18:39 +0000

    added part of the function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,13 @@
 // Declare variables
+const mainScreen = document.getElementById("start-screen");
+const startBtn = document.getElementById("start");
+
 const questionsContainer = document.getElementById("questions");
 const question = document.getElementById("question-title");
 const choices = document.getElementById("choices");
 const choicesText = Array.from(document.getElementsByClassName("choices"));
-const startBtn = document.getElementById("start");
 
 // Pressing start button will will hide the origin main text and populate questions and choices.
+startBtn.addEventListener("click", function () {
+  mainScreen.style.display = "none";
+});
