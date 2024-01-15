12135002925781d297084a9114d1fb905b523c43
Digita1Panda <prodigykyo@gmail.com>
2024-01-15 02:21:53 +0000

    added more variables

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,6 +7,11 @@ const question = document.getElementById("question-title");
 const choices = document.getElementById("choices");
 const choicesText = Array.from(document.getElementsByClassName("choices"));
 
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+
 // Pressing start button will will hide the origin main text and populate questions and choices.
 startBtn.addEventListener("click", function () {
   mainScreen.style.display = "none";
